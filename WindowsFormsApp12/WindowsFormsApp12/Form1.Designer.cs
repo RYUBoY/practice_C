21d1fd1928d33eb0cbf9a517b09042435ffaeff5
RYUBoY <ryusla@naver.com>
2024-07-19 23:13:27 +0900

    c# 3주차

--- WindowsFormsApp12/WindowsFormsApp12/Form1.Designer.cs
+++ WindowsFormsApp12/WindowsFormsApp12/Form1.Designer.cs
@@ -28,23 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.id = new System.Windows.Forms.TextBox();
+            this.open = new System.Windows.Forms.Button();
+            this.pw = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // id
+            // 
+            this.id.BackColor = System.Drawing.SystemColors.Window;
+            this.id.Location = new System.Drawing.Point(277, 156);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(221, 25);
+            this.id.TabIndex = 0;
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(82, 143);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 1;
+            this.open.Text = "open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // pw
+            // 
+            this.pw.Location = new System.Drawing.Point(277, 227);
+            this.pw.Name = "pw";
+            this.pw.Size = new System.Drawing.Size(221, 25);
+            this.pw.TabIndex = 2;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(277, 281);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(221, 43);
+            this.login.TabIndex = 3;
+            this.login.Text = "로그인";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Multiline = true;
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(277, 132);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(776, 426);
-            this.textBox1.TabIndex = 0;
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(82, 18);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "ID";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(277, 203);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(102, 18);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "PASSWORD";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.pw);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.id);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,7 +113,12 @@
 
         #endregion
 
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.TextBox pw;
+        private System.Windows.Forms.Button login;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- WindowsFormsApp12/WindowsFormsApp12/Form1.cs
+++ WindowsFormsApp12/WindowsFormsApp12/Form1.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -53,29 +55,197 @@ namespace WindowsFormsApp12
             //{
             //    textBox1.Text += e.Message;
             //}
+
+            //List<string> list = new List<string>();
+            //list.Add("일");
+            //list.Add("이");
+            //list.Add("삼");
+
+            //point(list);
+
+
+            //void point(List<string> arr)
+            //{
+            //    if (arr.Count == 0)
+            //        return;
+
+            //    int length = arr.Count-1;
+            //    textBox1.Text += arr[length] + "\r\n";
+            //    arr.RemoveAt(length);
+
+
+            //    point(arr);
+
+            //}
+
+            //Hashtable hashtable = new Hashtable();
+
+            //List<string> list = new List<string>();
+            //list.Add("1");
+            //list.Add("2");
+            //string[] arr = ;
+            //hashtable[0] = 999;
+            //hashtable["이름"] = "조은";
+            //hashtable["취미"] = "게에이임";
+
+            //textBox1.Text += $"{hashtable[0]}\r\n";
+            //textBox1.Text += $"{hashtable["이름"]}\r\n";
+            //textBox1.Text += $"{hashtable["취미"]}\r\n";
+
+            //Stack<int> i_stack = new Stack<int>();
+            //i_stack.Push(1);
+            //int num = i_stack.Pop();
+            //textBox1.Text += $"{num}\r\n";
+
+            //List<double> d_list = new List<double>();
+            //d_list.Add(30.409);
+            //d_list.Add(329.329);
+
+            //foreach (double d in d_list)
+            //{
+            //    textBox1.Text += $"{d}\r\n";
+            //}
+
+            //Weekday today = Weekday.Monday;
+
+            //int size = 10;
+            //var numbers = Enumerable.Range(1, size);
+            //var same_numbers = Enumerable.Repeat(0, size);
+            //var numbers_array = Enumerable.Range(1, size).ToArray();
+
+            //textBox1.Text += $"{numbers} \r\n";
+            //textBox1.Text += $"{same_numbers} \r\n";
+
+            //foreach ( var number in numbers )
+            //{
+            //    textBox1.Text += $"{number} ";
+            //}
+
+            //textBox1.Text += "\r\n" + string.Join(" ", same_numbers);
+            //textBox1.Text += "\r\n";
+
+            //foreach (var number in Enumerable.Range(1,10))
+            //{
+            //    textBox1.Text += $"{number} ";
+            //}
+
+            //IDictionary<string, string> data1 = new Dictionary<string, string>();
+            //var data2 = new Dictionary<string, string>();
+
+            //data1.Add("오리온", "고래밥");
+            //data1.Add("농심", "신라면");
+
+            //data1.Remove("오리온");
+            //data2["농심"] = "너구리";
+
+            //try
+            //{
+            //    data2.Add("농심", "짜파게티");
+            //}
+            //catch(Exception ex)
+            //{
+            //    textBox1.Text = ex.Message;
+            //}
+            //finally
+            //{
+            //    textBox1.Text = data2["농심"];
+            //}
+
+            //if (!data1.ContainsKey("오리온"))
+            //{
+            //    data1.Add("오리온", "초코파이");
+            //}
+
+            //if (data1.TryGetValue("오리온", out string value))
+            //{
+            //    textBox1.Text += $"{value}\r\n";
+            //}
+
+            //foreach (KeyValuePair<string, string> item in data1)
+            //{
+            //    textBox1.Text += $"{item.Key}에서 {item.Value}를 만듭니다.\r\n";
+            //}
+
             
-            List<string> list = new List<string>();
-            list.Add("일");
-            list.Add("이");
-            list.Add("삼");
+        }
+        IDictionary<string,ID> IDPW = new Dictionary<string, ID>();
+        //IDictionary<string, string> IDPN = new Dictionary<string, string>();
+        
+
+        public enum Weekday
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        struct ID
+        {
+            public string x;
+            public string y;
             
-            point(list);
+        }
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
             
 
-            void point(List<string> arr)
+            if (ofd.ShowDialog() == DialogResult.OK)
             {
-                if (arr.Count == 0)
-                    return;
+                
+                var fileStream = ofd.OpenFile();
 
-                int length = arr.Count-1;
-                textBox1.Text += arr[length] + "\r\n";
-                arr.RemoveAt(length);
+                using (StreamReader sr = new StreamReader(fileStream))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        ID ID = new ID();
+                        string[] words = line.Split(',');
+                        ID.x = words[1];
+                        if(words[2] == "")
+                        {
+                            ID.y = null;
+                        }
+                        else
+                        {
+                            ID.y = words[2];
+                        }
 
-               
-                point(arr);
+                        IDPW.Add(words[0], ID);
+                    }
+                
+                }
+                
+            }
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+
+
+
+            if (!IDPW.ContainsKey(id.Text))
+            {
+                MessageBox.Show("아이디가 없습니다.");
+            }
+            else
+            {
+                if (IDPW[id.Text].x != pw.Text)
+                {
+                    MessageBox.Show("비밀번호가 틀렸습니다.");
+                }
+                else
+                {
+
+                    MessageBox.Show($"로그인 성공 \r\n\n 아이디 : {id.Text} \r\n 전화번호 : {IDPW[id.Text].y}");
+                }
 
             }
-
         }
     }
 }
