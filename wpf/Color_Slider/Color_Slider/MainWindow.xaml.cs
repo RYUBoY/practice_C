9d02325b0b26db90db3d695faff4785fe9b75213
RYUBoY <ryusla@naver.com>
2024-07-26 13:59:12 +0900

    7.26 교육내용

---
+++ wpf/Color_Slider/Color_Slider/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Color_Slider
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            
+        }
+
+        private void r_slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            red.Content = r_slider.Value.ToString();
+            bacground_color();
+        }
+
+        private void g_slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            green.Content = g_slider.Value.ToString();
+            bacground_color();
+        }
+
+        private void b_slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            blue.Content = b_slider.Value.ToString();
+            bacground_color();
+        }
+
+        private void bacground_color()
+        {
+            double r = r_slider.Value;
+            double g = g_slider.Value;
+            double b = b_slider.Value;
+            if (Original.IsChecked == true)
+            {
+                
+                Main.Background = new SolidColorBrush(Color.FromRgb((byte)r, (byte)g, (byte)b));
+            }
+            
+            else if (Gray_Tone.IsChecked == true)
+            {
+                double avg = (r + g + b) / 3;
+                Main.Background = new SolidColorBrush(Color.FromRgb((byte)avg, (byte)avg, (byte)avg));
+            }
+            else if (Invert.IsChecked == true)
+            {
+                r = 255 - r;
+                g = 255 - g;
+                b = 255 - b;
+                Main.Background = new SolidColorBrush(Color.FromRgb((byte)r, (byte)g, (byte)b));
+            }
+        }
+
+        private void Original_Checked(object sender, RoutedEventArgs e)
+        {
+            r_slider.Value = 0;
+            g_slider.Value = 0;
+            b_slider.Value = 0;
+            Main.Background = new SolidColorBrush(Color.FromRgb((byte)r_slider.Value, (byte)g_slider.Value, (byte)b_slider.Value));
+        }
+
+        private void Gray_Tone_Checked(object sender, RoutedEventArgs e)
+        {
+            r_slider.Value = 0;
+            g_slider.Value = 0;
+            b_slider.Value = 0;
+            Main.Background = new SolidColorBrush(Color.FromRgb((byte)r_slider.Value, (byte)g_slider.Value, (byte)b_slider.Value));
+        }
+
+        private void Invert_Checked(object sender, RoutedEventArgs e)
+        {
+            r_slider.Value = 0;
+            g_slider.Value = 0;
+            b_slider.Value = 0;
+            Main.Background = new SolidColorBrush(Color.FromRgb((byte)r_slider.Value, (byte)g_slider.Value, (byte)b_slider.Value));
+        }
+    }
+}
---
+++ wpf/WpfApp5/WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void RadioButton1_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            //if (RadioButton1.IsChecked == true)
+            {
+                MessageBox.Show($"good! {radioButton.IsChecked}");
+            }
+        }
+
+        private void RadioButton1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            //if(RadioButton1.IsChecked == false)
+            {
+                MessageBox.Show("Bad!");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ////Button button = (Button)sender;
+            ////MessageBox.Show($"Button Check {button.Content}");
+            //comboBox.Items.Add("tomato juice"); 
+            //ComboBoxItem item = (ComboBoxItem)comboBox.SelectedValue;
+            //string item_name = item.Content.ToString();
+            //comboBox.Items.Remove(comboBox.SelectedItem);
+            listBox.Items.Add("악어");
+            //listBox.Items.Add("병아리");
+            //listBox.Items.Add("펭귄");
+
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Slider slider = (Slider)sender;
+            label1.Content = slider.Value.ToString();
+        }
+
+        private void web_button_Click(object sender, RoutedEventArgs e)
+        {
+            //web.Navigate("http://naver.com");
+            //web.GoBack();
+            //web.GoForward();
+            //web.CoreWebView2.Navigate("http://www.naver.com");
+        }
+
+        private void Go_Click(object sender, RoutedEventArgs e)
+        {
+            
+            web.CoreWebView2.Navigate(url.Text);
+        }
+    }
+}
